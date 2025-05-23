b069c850d06c81b6fc434b7e0bf490e69fa6bd85
mstf74 <mostafa3333gg@gmail.com>
2025-05-23 15:18:25 +0300

    final game

--- Assets/Scripts/WeaponSwitch.cs
+++ Assets/Scripts/WeaponSwitch.cs
@@ -6,12 +6,10 @@ public class WeaponsSwitch : MonoBehaviour
 {
     public GameObject object01;
     public GameObject object02;
-    public GameObject object03;
     void Start()
     {
         object01.SetActive(true);
         object02.SetActive(false);
-        object03.SetActive(false);
     }
 
     void Update()
@@ -20,21 +18,18 @@ public class WeaponsSwitch : MonoBehaviour
         {
             object01.SetActive(true);
             object02.SetActive(false);
-            object03.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             object01.SetActive(false);
             object02.SetActive(true);
-            object03.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             object01.SetActive(false);
             object02.SetActive(false);
-            object03.SetActive(true);
         }
     }
 }
